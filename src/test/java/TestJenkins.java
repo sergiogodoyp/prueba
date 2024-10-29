1a46e20c06409e717908a7671caa4df1becbd224
Sergio Godoy <sergio_jgp@hotmail.com>
2024-10-28 21:48:30 -0500

    subir proyecto ejemplo

---
+++ src/test/java/TestJenkins.java
@@ -0,0 +1,48 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestJenkins {
+
+
+    protected static WebDriver driver = null;
+
+    @BeforeEach
+    void lanzarNavegador(){
+        driver = crearChromeDriver();
+        driver.manage().window().maximize();
+
+
+    }
+
+    @AfterEach
+    void cerrarNavegador(){
+        driver.quit();
+    }
+
+    @Test
+    void test01(){
+        driver.get("http://www.amazon.com");
+    }
+
+    @Test
+    void test02(){
+        driver.get("https://the-internet.herokuapp.com/");
+        WebElement formAuthenticationLink = driver.findElement(By.linkText("Form Authentication"));
+        formAuthenticationLink.click();
+        WebElement username = driver.findElement(By.id("username"));
+        username.sendKeys("tomsmith");
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("SuperSecretPassword!");
+        WebElement login = driver.findElement(By.className("radius"));
+        login.submit();
+    }
+
+    public static WebDriver crearChromeDriver(){
+        return new ChromeDriver();
+    }
+}
