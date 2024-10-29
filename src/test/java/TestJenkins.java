31e59597fd350797d57f96243f2ed495fbd9ef6c
Sergio Godoy <sergio_jgp@hotmail.com>
2024-10-28 21:58:11 -0500

    subir cambios

--- src/test/java/TestJenkins.java
+++ src/test/java/TestJenkins.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class TestJenkins {
 
 
@@ -40,6 +42,9 @@ public class TestJenkins {
         password.sendKeys("SuperSecretPassword!");
         WebElement login = driver.findElement(By.className("radius"));
         login.submit();
+        String mensajeActual = driver.findElement(By.className("success")).getText();
+        String mensajeEsperado = "You logged into a secure area!";
+        assertTrue(mensajeActual.contains(mensajeEsperado), "Login Incorrecto");
     }
 
     public static WebDriver crearChromeDriver(){
